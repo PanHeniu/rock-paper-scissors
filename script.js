43640453aee389d93ee5a1703356deabf461d004
PanHeniu <dallaskomputerek@protonmail.com>
2022-07-01 21:41:27 +0200

    Removed the old script file. Updated the name of the new one.

--- script.js
+++ script.js
@@ -1,5 +1,65 @@
-function computerPlay() { // this function randomly returns either rock, paper or scissors
-    let randomSelection = Math.floor(Math.random() * 3); // Math.random() generates and returns a random float in the range: 0 to < 1 (but not 1!). Math.floor() rounds this number downwards to the closest integer. Times three, so that the random number is in the range: 0 to < 3. So, possible results are: 0, 1 and 2. 
+let roundNo = document.getElementById('round-no');
+let roundPlayer = document.getElementById('player-select');
+let roundComputer = document.getElementById('computer-select');
+let roundResult = document.getElementById('round-result');
+let roundScore = document.getElementById('score');
+let roundTotalResult = document.getElementById('total-result');
+
+let rock = document.getElementById('rock');
+let paper = document.getElementById('paper');
+let scissors = document.getElementById('scissors');
+let playAgain = document.getElementById('reset');
+
+// Variables to keep track of scores and rounds. 
+let rounds = 0;
+let playerScore = 0;
+let computerScore = 0;
+
+rock.addEventListener('click', game);
+paper.addEventListener('click', game);
+scissors.addEventListener('click', game);
+
+// The main game function. 
+function game (e) {
+    const computerSelection = computerPlay();
+    const playerSelection = e.target.id; // rock, paper, scissors value comes from the element's id in html
+    
+    // adds the CSS 'clicked' class
+    const item = document.querySelector(`#${playerSelection}`);
+    item.classList.add('clicked');
+
+    const round = playRound(playerSelection, computerSelection);
+    if (playerScore < 5 && computerScore < 5) {
+        roundNo.textContent = `ROUND: ${rounds + 1}`;
+        roundPlayer.textContent = 'Player: ' + playerSelection;
+        roundComputer.textContent = 'Computer: ' + computerSelection;
+        roundResult.textContent = round;
+    
+        if (round.includes('Win!')) {
+            roundScore.textContent = `Player: ${++playerScore} Computer: ${computerScore}`;
+            rounds++;
+        } else if (round.includes('Lose!')) {
+            roundScore.textContent = `Player: ${playerScore} Computer: ${++computerScore}`;
+            rounds++;
+        } else {
+            rounds++;
+        }
+    }
+    totalScore();
+}
+
+// Runs when one of the players has scored 5 points i.e. won the game.
+function totalScore () {
+    if (playerScore === 5 && playerScore > computerScore) {
+        roundTotalResult.textContent = `You won! Congrats!`;
+    } else if (computerScore === 5 && playerScore < computerScore) {
+        roundTotalResult.textContent = `You lost! Better luck next time!`;
+    }
+}
+
+// Assigns a random value from 0 to 2 and chooses one of the elements based on that.
+function computerPlay() {
+    let randomSelection = Math.floor(Math.random() * 3);
     if (randomSelection === 0) {
         return 'rock';
     } else if (randomSelection === 1) {
@@ -7,26 +67,14 @@ function computerPlay() { // this function randomly returns either rock, paper o
     } else {
         return 'scissors';
     }
-    //console.log(randomSelection);
 }
 
-function playerPlay() { // the function prompts player to enter rock, paper or scissors 
-    while (true) {
-        let value = (prompt('Rock, paper, scissors?')).toLowerCase(); // matches the computer's case i.e. lower
-        //console.log(value);
-        if (value === 'rock' || value === 'paper' || value === 'scissors') {
-            return value; // returns only one of these values; otherwise prompts again
-        }
-    }
-}
-
-
-function playRound(playerSelection, computerSelection) { // function compares different vaues and determines the winner of the round
+function playRound(playerSelection, computerSelection) {
     if (playerSelection === 'rock' && computerSelection === 'paper') {
         return 'You Lose! Paper beats Rock.';
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
         return 'You Win! Rock beats Scissors.';
-    } else if (playerSelection === computerSelection) { // TIE
+    } else if (playerSelection === computerSelection) {
         return `It's a Tie. You both chose ${playerSelection}.`;
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
         return 'You Win! Paper beats Rock.';
@@ -39,35 +87,26 @@ function playRound(playerSelection, computerSelection) { // function compares di
     }
 }
 
-function game() { // plays a 5 round game that keeps score and reports a winner or loser at the end
-    let playerScore = 0; // variables to keep score
-    let computerScore = 0;
-    
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = playerPlay(); // keeps the result of the selection function in a variable, so it can be used later (e.g. to show what was chosen)
-        const computerSelection = computerPlay();
-        
-        const round = playRound(playerSelection, computerSelection);
-        console.log(`ROUND: ${i + 1}`);
-        console.log('Player: ' + playerSelection);
-        console.log('Computer: ' + computerSelection);
-        console.log(round);
+const box = document.querySelectorAll('.box');
+box.forEach(box => box.addEventListener('transitionend', removeClicked));
 
-        if (round.includes('Win!')) { // if the string from the playRounf function returns a string that contains 'Win!' increment player score by 1
-            playerScore++;
-        }
-        else if (round.includes('Lose!')) {
-            computerScore++;
-        }
+// Removes the CSS 'clicked' class.
+function removeClicked(e) {
+    if (e.propertyName !== 'transform') {
+        return;
     }
-
-    if (playerScore > computerScore) {
-        console.log(`The total score is ${playerScore}:${computerScore}. You won! Congrats!`);
-    } else if (playerScore < computerScore) {
-        console.log(`The total score is ${playerScore}:${computerScore}. You lost! Better luck next time!`);
-    } else {
-        console.log(`The total score is ${playerScore}:${computerScore}. Must be a tie!`);
-    }
+    this.classList.remove('clicked');
 }
 
-game();
+// Resets everything after clicking the 'play again' button.
+playAgain.addEventListener('click', function() {
+    rounds = 0;
+    playerScore = 0;
+    computerScore = 0;
+    roundNo.textContent = '';
+    roundPlayer.textContent = '';
+    roundComputer.textContent = '';
+    roundResult.textContent = '';
+    roundScore.textContent = '';
+    roundTotalResult.textContent = '';
+})
